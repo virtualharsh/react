e60d7157bfe0775eb9a362dbfc4103e77b9d571e
harsh <harshsonegrahere@gmail.com>
2025-01-09 17:34:04 +0530

    modified custom hook project

--- 7 Custom Hooks/custom-hooks/src/components/InputBox.jsx
+++ 7 Custom Hooks/custom-hooks/src/components/InputBox.jsx
@@ -1,57 +1,54 @@
-import { React, useEffect, useId } from "react";
-
+import { useId, useState } from "react";
 
 function InputBox({
     label,
-    onAmountChange,
-    onCurrencyChange,
+    amountChange,
+    currencyChange,
     currencyOptions = [],
-    amount="",
-    selectedCurrency = "usd",
+    selectedCurrency,
+    amountDisabled = false,
+    amount=0,
     className = "",
 }) {
-    const amountInputId = useId()
 
-    
-
-
+    const id = useId()
+    const [currency,setCurrency] = useState(selectedCurrency)
     return (
+        
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
+                <label htmlFor={id} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
+
                 <input
-                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
+                    id={id}
                     type="number"
-                    placeholder="Amount"
                     value={amount}
-                    onChange={(e)=> onAmountChange && onAmountChange(e.target.value)}
+                    disabled={amountDisabled}
+                    onChange={(e)=>{amountChange && amountChange(e.target.value)}}
+                    placeholder="Amount"
                 />
             </div>
-            
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    value={selectedCurrency}
-                    onChange={(e) => {
-                        if (onCurrencyChange) {
-                            onCurrencyChange(e.target.value); // Update the parent state
-                        }else{
-                            console.log("error");
-                            
-                        }
+                    value={currency}
+                    onChange={(e)=>{currencyChange && currencyChange(e.target.value);4
+                        setCurrency(e.target.value)
                     }}
+                    
                 >
-                    {currencyOptions.map((currency) => (
-                        <option value={currency} key={currency}>
-                            {currency.toUpperCase()}
-                        </option>
-                    ))}
-                </select>
 
+                    {currencyOptions.map((val,index)=>
+                        <option key={index} value={val} >
+                            {val}
+                        </option>
+                    )}
+
+                </select>
             </div>
         </div>
     );
